ec870acafae82f5371e5d351030472868a7cd776
matthcol <matthias.colin@gmail.com>
2025-10-07 10:15:23 +0200

    tests pour add et radd

--- 03-OopClassic/tests/test_point.py
+++ 03-OopClassic/tests/test_point.py
@@ -29,8 +29,71 @@ def test_eq_when_other_not_a_point(point_1):
 def test_ne():
     pass
 
-def test_another():
-    pass
+
+def test_add_with_scalar(point_1):
+    point = point_1 + 2.5
+    assert point.x == 6.0
+    assert point.y == 4.75
+
+def test_add_with_tuple(point_1):
+    point = point_1 + (2.5, 2.25)
+    assert point.x == 6.0
+    assert point.y == 4.5
+
+def test_add_with_point(point_1):
+    point = point_1 + Point(2.5, 2.25)
+    assert point.x == 6.0
+    assert point.y == 4.5
+
+@pytest.mark.parametrize(
+        "rhs",
+        [
+            "a text",
+            False,
+            None
+        ],
+        ids=[
+            "str",
+            "bool",
+            "None"
+        ]
+)
+def test_add_with_unsupported_type(point_1, rhs):
+    with pytest.raises(TypeError):
+        # hook: __enter__
+        _ = point_1 + rhs
+    # hook __exit__ : check if exception TypeError happens
+
+
+def test_radd_with_scalar(point_1):
+    point = 2.5 + point_1
+    assert point.x == 6.0
+    assert point.y == 4.75
+
+def test_radd_with_tuple(point_1):
+    point = (2.5, 2.25) + point_1
+    assert point.x == 6.0
+    assert point.y == 4.5
+
+@pytest.mark.parametrize(
+        "rhs",
+        [
+            "a text",
+            False,
+            None
+        ],
+        ids=[
+            "str",
+            "bool",
+            "None"
+        ]
+)
+def test_radd_with_unsupported_type(point_1, rhs):
+    with pytest.raises(TypeError):
+        # hook: __enter__
+        _ = rhs + point_1 
+    # hook __exit__ : check if exception TypeError happens
+
 
 def test_iadd_with_scalar(point_1):
     point_1 += 2.5
